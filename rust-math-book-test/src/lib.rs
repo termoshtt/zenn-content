08ac049b9a37aef41e14c1bbac6c4aa275838497
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-06-08 01:43:22 +0900

    minor

--- rust-math-book-test/src/lib.rs
+++ rust-math-book-test/src/lib.rs
@@ -28,6 +28,10 @@
 //! -----------------------
 //! Book本文中の各章毎にモジュールを作り、それのdocstringとしてテストを行います
 //!
+//! 注意
+//! -----
+//! 依存しているライブラリは **この** crateの`[dependencies]`に追加されています
+//!
 
 #[doc = include_str!("../../books/b4bce1b9ea5e6853cb07/ndarray_linalg.md")]
-pub mod linalg {}
+pub mod ndarray_linalg {}
