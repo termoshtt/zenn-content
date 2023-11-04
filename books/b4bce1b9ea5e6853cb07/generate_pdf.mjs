a831a7bdf402e5ee43d69e5404510f73968307c6
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-11-04 17:14:56 +0900

    Refactor generate_pdf.mjs to include multiple pages

--- books/b4bce1b9ea5e6853cb07/generate_pdf.mjs
+++ books/b4bce1b9ea5e6853cb07/generate_pdf.mjs
@@ -6,11 +6,36 @@ import { mdToPdf } from 'md-to-pdf';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const target = path.join(__dirname, "intro.md");
+const page_break = '\n<div class="page-break"></div>\n'
+
+function asTitle(title) {
+    return `\n# ${title}\n`;
+}
+
+const about = fs.readFileSync(path.join(__dirname, "about.md"), { encoding: 'utf8' });
+const intro = fs.readFileSync(path.join(__dirname, "intro.md"), { encoding: 'utf8' });
+
+function trimHeaders(content) {
+    const lines = content.split('\n');
+    const title = lines[1].replace(/^title:/, '').trim();
+    lines.splice(0, 3);
+    return [title, lines.join('\n')];
+}
+
+let contents = '';
+
+['about', 'intro'].forEach(page => {
+    const raw = fs.readFileSync(path.join(__dirname, `${page}.md`), { encoding: 'utf8' });
+    const [title, body] = trimHeaders(raw);
+    contents += asTitle(title);
+    contents += body;
+    contents += page_break;
+});
+
+console.log(contents);
 
 (async () => {
-    const pdf = await mdToPdf({ path: target }).catch(console.error);
-
+    const pdf = await mdToPdf({ content: contents }).catch(console.error);
     if (pdf) {
         fs.writeFileSync('book.pdf', pdf.content);
     }
